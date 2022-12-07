723e329d7afaace457ad9f36aeb138facb1f467f
s1822284 <t.g.sierink@student.utwente.nl>
2022-12-07 13:01:54 +0100

    day111

--- day1.py
+++ day1.py
@@ -1,7 +1,8 @@
 f = open("input/day1.txt", "r").read().split('\n\n')
-#comment
+#Comment222
 f = [line.split('\n') for line in f]
 
 f = [[int(n) for n in line] for line in f]
 
 print(max([sum(line) for line in f]))
+print(max(f))
